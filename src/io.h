1f40fedb6dd81aa536ade26983665a16bab351b8
Pierre Paleo <pierre.paleo@esrf.fr>
2016-09-21 16:50:52 +0200

    corrected a compilation issue for float-double

--- src/io.h
+++ src/io.h
@@ -1,9 +1,9 @@
 #ifndef IO_H
 #define IO_H
 
-DTYPE* read_dat_file_DTYPE(const char* fname, int len);
+float* read_dat_file_float(const char* fname, int len);
 
-void write_dat_file_DTYPE(const char* fname, DTYPE* arr, int len);
+void write_dat_file_float(const char* fname, float* arr, int len);
 
 int prompt_text(char* input);
 
