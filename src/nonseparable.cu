d7cb8ca8944e2aa0d55549f4add94829f60dd972
Pierre Paleo <pierre.paleo@esrf.fr>
2016-11-14 17:01:34 +0100

    should be OK for non-separable DWT

--- src/nonseparable.cu
+++ src/nonseparable.cu
@@ -80,7 +80,12 @@ int w_compute_filters(const char* wname, int direction, int do_swt) {
 __global__ void w_kern_forward(DTYPE* img, DTYPE* c_a, DTYPE* c_h, DTYPE* c_v, DTYPE* c_d, int Nr, int Nc, int hlen) {
     int gidx = threadIdx.x + blockIdx.x*blockDim.x;
     int gidy = threadIdx.y + blockIdx.y*blockDim.y;
-    int Nr2 = Nr/2, Nc2 = Nc/2;
+
+    int Nr_is_odd = (Nr & 1);
+    int Nr2 = (Nr + Nr_is_odd)/2;
+    int Nc_is_odd = (Nc & 1);
+    int Nc2 = (Nc + Nc_is_odd)/2;
+
     if (gidy < Nr2 && gidx < Nc2) {
         int c, hL, hR;
         if (hlen & 1) { // odd kernel size
@@ -93,10 +98,6 @@ __global__ void w_kern_forward(DTYPE* img, DTYPE* c_a, DTYPE* c_h, DTYPE* c_v, D
             hL = c;
             hR = c+1;
         }
-        int jy1 = c - 2*gidy;
-        int jy2 = Nr - 1 - 2*gidy + c;
-        int jx1 = c - 2*gidx;
-        int jx2 = Nc - 1 - 2*gidx + c;
         DTYPE res_a = 0, res_h = 0, res_v = 0, res_d = 0;
         DTYPE img_val;
 
@@ -104,12 +105,21 @@ __global__ void w_kern_forward(DTYPE* img, DTYPE* c_a, DTYPE* c_h, DTYPE* c_v, D
         // The following can be sped-up by splitting into 3*3 loops, but it would be a nightmare for readability
         for (int jy = 0; jy <= hR+hL; jy++) {
             int idx_y = gidy*2 - c + jy;
-            if (jy < jy1) idx_y += Nr;
-            if (jy > jy2) idx_y -= Nr ;
+            if (idx_y < 0) idx_y += (Nr + Nr_is_odd); // if N is odd, image is virtually extended
+            // no "else if", since idx_y can be > N-1  after being incremented
+            if (idx_y > Nr-1) {
+                if ((idx_y == Nr) && (Nr_is_odd)) idx_y--; // if N is odd, repeat the right-most element
+                else idx_y -= (Nr + Nr_is_odd); // if N is odd, image is virtually extended
+            }
+
             for (int jx = 0; jx <= hR+hL; jx++) {
                 int idx_x = gidx*2 - c + jx;
-                if (jx < jx1) idx_x += Nc;
-                if (jx > jx2) idx_x -= Nc ;
+                if (idx_x < 0) idx_x += (Nc + Nc_is_odd); // if N is odd, image is virtually extended
+                // no "else if", since idx_x can be > N-1  after being incremented
+                if (idx_x > Nc-1) {
+                    if ((idx_x == Nc) && (Nc_is_odd)) idx_x--; // if N is odd, repeat the right-most element
+                    else idx_x -= (Nc + Nc_is_odd); // if N is odd, image is virtually extended
+                }
 
                 img_val = img[idx_y*Nc + idx_x];
                 res_a += img_val * c_kern_LL[(hlen-1-jy)*hlen + (hlen-1 - jx)];
@@ -118,10 +128,10 @@ __global__ void w_kern_forward(DTYPE* img, DTYPE* c_a, DTYPE* c_h, DTYPE* c_v, D
                 res_d += img_val * c_kern_HH[(hlen-1-jy)*hlen + (hlen-1 - jx)];
             }
         }
-        c_a[(gidy)* Nc2 + (gidx)] = res_a;
-        c_h[(gidy)* Nc2 + (gidx)] = res_h;
-        c_v[(gidy)* Nc2 + (gidx)] = res_v;
-        c_d[(gidy)* Nc2 + (gidx)] = res_d;
+        c_a[gidy* Nc2 + gidx] = res_a;
+        c_h[gidy* Nc2 + gidx] = res_h;
+        c_v[gidy* Nc2 + gidx] = res_v;
+        c_d[gidy* Nc2 + gidx] = res_d;
     }
 }
 
@@ -129,11 +139,9 @@ __global__ void w_kern_forward(DTYPE* img, DTYPE* c_a, DTYPE* c_h, DTYPE* c_v, D
 
 
 // must be run with grid size = (2*Nr, 2*Nc) ; Nr = numrows of input
-__global__ void w_kern_inverse(DTYPE* img, DTYPE* c_a, DTYPE* c_h, DTYPE* c_v, DTYPE* c_d, int Nr, int Nc, int hlen) {
+__global__ void w_kern_inverse(DTYPE* img, DTYPE* c_a, DTYPE* c_h, DTYPE* c_v, DTYPE* c_d, int Nr, int Nc, int Nr2, int Nc2, int hlen) {
     int gidx = threadIdx.x + blockIdx.x*blockDim.x;
     int gidy = threadIdx.y + blockIdx.y*blockDim.y;
-    int Nr2 = Nr*2, Nc2 = Nc*2;
-     //~ if ((gidy < Nr2-10 && gidx < Nc2-10) && (gidx > 10 && gidy > 10)) {
     if (gidy < Nr2 && gidx < Nc2) {
 
         int c, hL, hR;
@@ -192,7 +200,9 @@ int w_forward(DTYPE* d_image, DTYPE** d_coeffs, DTYPE* d_tmp, w_info winfos) {
 
     int Nr = winfos.Nr, Nc = winfos.Nc, levels = winfos.nlevels, hlen = winfos.hlen;
     int tpb = 16; // TODO : tune for max perfs.
-    int Nc2 = Nc/2, Nr2 = Nr/2;
+    int Nc2 = Nc, Nr2 = Nr;
+    int Nc2_old = Nc2, Nr2_old = Nr2;
+    w_div2(&Nr2); w_div2(&Nc2);
     DTYPE* d_tmp1, *d_tmp2;
     d_tmp1 = d_coeffs[0];
     d_tmp2 = d_tmp;
@@ -200,25 +210,31 @@ int w_forward(DTYPE* d_image, DTYPE** d_coeffs, DTYPE* d_tmp, w_info winfos) {
     // First level
     dim3 n_blocks = dim3(w_iDivUp(Nc2, tpb), w_iDivUp(Nr2, tpb), 1);
     dim3 n_threads_per_block = dim3(tpb, tpb, 1);
-    w_kern_forward<<<n_blocks, n_threads_per_block>>>(d_image, d_coeffs[0], d_coeffs[1], d_coeffs[2], d_coeffs[3], Nr2*2, Nc2*2, hlen);
+    w_kern_forward<<<n_blocks, n_threads_per_block>>>(d_image, d_coeffs[0], d_coeffs[1], d_coeffs[2], d_coeffs[3], Nr, Nc, hlen);
 
     for (int i=1; i < levels; i++) {
-        Nc2 /= 2;
-        Nr2 /= 2;
+        Nr2_old = Nr2; Nc2_old = Nc2;
+        w_div2(&Nr2); w_div2(&Nc2);
         n_blocks = dim3(w_iDivUp(Nc2, tpb), w_iDivUp(Nr2, tpb), 1);
-        w_kern_forward<<<n_blocks, n_threads_per_block>>>(d_tmp1, d_tmp2, d_coeffs[3*i+1], d_coeffs[3*i+2], d_coeffs[3*i+3], Nr2*2, Nc2*2, hlen);
+        w_kern_forward<<<n_blocks, n_threads_per_block>>>(d_tmp1, d_tmp2, d_coeffs[3*i+1], d_coeffs[3*i+2], d_coeffs[3*i+3], Nr2_old, Nc2_old, hlen);
         w_swap_ptr(&d_tmp1, &d_tmp2);
     }
-    if ((levels & 1) == 0) cudaMemcpy(d_coeffs[0], d_tmp, Nr2*Nc2*sizeof(DTYPE), cudaMemcpyDeviceToDevice);
+    if ((levels > 1) && ((levels & 1) == 0)) cudaMemcpy(d_coeffs[0], d_tmp, Nr2*Nc2*sizeof(DTYPE), cudaMemcpyDeviceToDevice);
     return 0;
 }
 
 
 int w_inverse(DTYPE* d_image, DTYPE** d_coeffs, DTYPE* d_tmp, w_info winfos) {
     int Nr = winfos.Nr, Nc = winfos.Nc, levels = winfos.nlevels, hlen = winfos.hlen;
-    int Nr0 = Nr, Nc0 = Nc;
-    Nr /= w_ipow2(levels);
-    Nc /= w_ipow2(levels);
+     // Table of sizes. FIXME: consider adding this in the w_info structure
+    int tNr[levels+1]; tNr[0] = Nr;
+    int tNc[levels+1]; tNc[0] = Nc;
+    for (int i = 1; i <= levels; i++) {
+        tNr[i] = tNr[i-1];
+        tNc[i] = tNc[i-1];
+        w_div2(tNr + i);
+        w_div2(tNc + i);
+    }
     int tpb = 16; // TODO : tune for max perfs.
 
     DTYPE* d_tmp1, *d_tmp2;
@@ -228,17 +244,14 @@ int w_inverse(DTYPE* d_image, DTYPE** d_coeffs, DTYPE* d_tmp, w_info winfos) {
     dim3 n_threads_per_block = dim3(tpb, tpb, 1);
     dim3 n_blocks;
     for (int i = levels-1; i >= 1; i--) {
-        n_blocks = dim3(w_iDivUp(Nc*2, tpb), w_iDivUp(Nr*2, tpb), 1);
-        w_kern_inverse<<<n_blocks, n_threads_per_block>>>(d_tmp2, d_tmp1, d_coeffs[3*i+1], d_coeffs[3*i+2], d_coeffs[3*i+3], Nr, Nc, hlen);
-        Nr *= 2;
-        Nc *= 2;
+        n_blocks = dim3(w_iDivUp(tNc[i], tpb), w_iDivUp(tNr[i], tpb), 1);
+        w_kern_inverse<<<n_blocks, n_threads_per_block>>>(d_tmp2, d_tmp1, d_coeffs[3*i+1], d_coeffs[3*i+2], d_coeffs[3*i+3], tNr[i+1], tNc[i+1], tNr[i], tNc[i], hlen);
         w_swap_ptr(&d_tmp1, &d_tmp2);
     }
-    if ((levels & 1) == 0) cudaMemcpy(d_coeffs[0], d_tmp, (Nr0/2)*(Nc0/2)*sizeof(DTYPE), cudaMemcpyDeviceToDevice);
-    //~ CUDACHECK;
+    if ((levels > 1) && ((levels & 1) == 0)) cudaMemcpy(d_coeffs[0], d_tmp, tNr[1]*tNc[1]*sizeof(DTYPE), cudaMemcpyDeviceToDevice);
     // First level
-    n_blocks = dim3(w_iDivUp(Nc*2, tpb), w_iDivUp(Nr*2, tpb), 1);
-    w_kern_inverse<<<n_blocks, n_threads_per_block>>>(d_image, d_coeffs[0], d_coeffs[1], d_coeffs[2], d_coeffs[3], Nr, Nc, hlen);
+    n_blocks = dim3(w_iDivUp(Nc, tpb), w_iDivUp(Nr, tpb), 1);
+    w_kern_inverse<<<n_blocks, n_threads_per_block>>>(d_image, d_coeffs[0], d_coeffs[1], d_coeffs[2], d_coeffs[3], tNr[1], tNc[1], Nr, Nc, hlen);
 
     return 0;
 }
--- src/nonseparable.h
+++ src/nonseparable.h
@@ -7,7 +7,7 @@ DTYPE* w_outer(DTYPE* a, DTYPE* b, int len);
 int w_compute_filters(const char* wname, int direction, int do_swt);
 
 __global__ void w_kern_forward(DTYPE* img, DTYPE* c_a, DTYPE* c_h, DTYPE* c_v, DTYPE* c_d, int Nr, int Nc, int hlen);
-__global__ void w_kern_inverse(DTYPE* img, DTYPE* c_a, DTYPE* c_h, DTYPE* c_v, DTYPE* c_d, int Nr, int Nc, int hlen);
+__global__ void w_kern_inverse(DTYPE* img, DTYPE* c_a, DTYPE* c_h, DTYPE* c_v, DTYPE* c_d, int Nr, int Nc, int Nr2, int Nc2, int hlen);
 int w_forward(DTYPE* d_image, DTYPE** d_coeffs, DTYPE* d_tmp, w_info winfos);
 int w_inverse(DTYPE* d_image, DTYPE** d_coeffs, DTYPE* d_tmp, w_info winfos);
 
